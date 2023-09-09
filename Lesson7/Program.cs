9a297b80d8237578c9e81c5a4adb9cb6f372f565
illia1304 <1304illia@gmail.com>
2023-09-09 15:42:19 +0300

    selection sort

--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -1,7 +1,43 @@
-﻿int n = int.Parse(Console.ReadLine());
-int[] array = new int[n];
+﻿int count = int.Parse(Console.ReadLine());
+int[] array = new int[count];
 
-for(int i = 0; i < n; i++)
+for(int i = 0; i < count; i++)
 {
     array[i] = int.Parse(Console.ReadLine());
+}
+
+SelectionSort(array);
+
+
+//selection
+void SelectionSort(int[] array)
+{
+    int length = array.Length;
+    int indexMin, temp;
+
+    for(int i=0; i < length - 1; i++){
+        indexMin = i;
+        for(int j = i + 1; j < length; j++)
+        {
+            if(array[j] < array[indexMin])
+                indexMin = j;
+        }
+        temp = array[i];
+        array[indexMin] = array[i];
+        array[i] = temp;
+    }
+}
+
+
+
+
+
+
+
+void printArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]+" ");
+    }
 }
